cac82d98710c6d612f6970604908a33486c9fab4
Filipe Husak <filipe.husak87@gmail.com>
2021-09-27 21:25:02 -0300

    encontro presencial 2

---
+++ encontro_presencial2.js
@@ -0,0 +1,10 @@
+var nAlunos = 14;
+for (let i = 0; i <= nAlunos; i++){
+    if(i == 0){
+        console.log("zero");
+    }else if(i % 2 == 0){
+        console.log("par");
+    }else{
+        console.log("impar");
+    }
+}
--- teste.js
+++ teste.js
@@ -1,3 +1,3 @@
-var x = [0, 1, 2, 3, 4];
-x = x.length;
+var x = [];
+x.push("yeh");
 console.log(x);
